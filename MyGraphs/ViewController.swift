b53b0912c963ffbde104606858804688b9ed17f5
liza.metla <liza.metla@gmail.com>
2020-07-02 14:59:25 +0300

    bar chart added with data chart entries (by using entries,set.data)

--- MyGraphs/ViewController.swift
+++ MyGraphs/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+//library for graphs
+import Charts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartViewDelegate {
 
+    var barChart = BarChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        barChart.delegate = self
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        //position on view controller
+        barChart.frame = CGRect(x: 0, y: 0,
+                                width: self.view.frame.size.width,
+                                height: self.view.frame.size.width)
+        
+        barChart.center = view.center
+        
+        view.addSubview(barChart)
+        
+        //data entries (ввод данных)
+        var entries = [BarChartDataEntry]()
+        
+        for x in 0..<10 {
+
+            entries.append(BarChartDataEntry(x: Double(x),
+                                             y: Double(x)))
+        }
+        
+        //data holder
+        let set = BarChartDataSet(entries: entries)
+        let data = BarChartData(dataSet: set)
+        
+        barChart.data = data
     }
 
 
