70a60677a89f6a9bbe6787564b6b425db7ba4337
liza.metla <liza.metla@gmail.com>
2020-07-06 19:03:55 +0300

    donut chart + colorful

--- MyGraphs/ThirdViewController.swift
+++ MyGraphs/ThirdViewController.swift
@@ -5,15 +5,49 @@
 //  Created by Елизавета Метла on 7/1/20.
 //  Copyright © 2020 Елизавета Метла. All rights reserved.
 //
-
+import Charts
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, ChartViewDelegate {
 
+    var donutChart = PieChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        donutChart.delegate = self
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+         //position on view controller
+               donutChart.frame = CGRect(x: 0, y: 0,
+                                       width: self.view.frame.size.width,
+                                       height: self.view.frame.size.width)
+               
+               donutChart.center = view.center
+               
+               view.addSubview(donutChart)
+               
+               //data entries (ввод данных)
+               var entries = [ChartDataEntry]()
+               
+               for x in 0..<15 {
+
+                   entries.append(ChartDataEntry(x: Double(x),
+                                                    y: Double(x)))
+               }
+               
+               //data holder
+               let set = PieChartDataSet(entries: entries)
+               
+               
+               set.colors = ChartColorTemplates.colorful()
+               
+               let data = PieChartData(dataSet: set)
+               
+               donutChart.data = data
     }
     
 
